220bd562b78a3707f1f701df3cbe5705c56a2bdf
Duxcoder <iinegve_25@mail.ru>
2023-10-31 08:56:25 +0300

    refactor: transform Sword to functional component

--- src/components/Spinners/Sword.tsx
+++ src/components/Spinners/Sword.tsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import cl from './Sword.module.css';
 import hilt from '../../assets/images/hilt.svg';
 
-class Sword extends Component {
-  render() {
-    return (
-      <div className={cl.spinner}>
-        <div className={cl.sword}>
-          <img src={hilt} alt="Sword loading" className={cl.hilt}></img>
-          <div className={cl.blade}>
-            <div className={cl.light}></div>
-          </div>
+const Sword = () => {
+  return (
+    <div className={cl.spinner}>
+      <div className={cl.sword}>
+        <img src={hilt} alt="Sword loading" className={cl.hilt}></img>
+        <div className={cl.blade}>
+          <div className={cl.light}></div>
         </div>
-        <span className={cl.text}>LOADING...</span>
       </div>
-    );
-  }
-}
+      <span className={cl.text}>LOADING...</span>
+    </div>
+  );
+};
 
 export default Sword;
