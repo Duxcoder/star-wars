8d50932c8b47c490d4e17714dbed761f1ed74796
Duxcoder <iinegve_25@mail.ru>
2023-10-31 09:31:20 +0300

    refactor: transform App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import MyContent from './components/MyContent/MyContent';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <MyContent />
-      </ErrorBoundary>
-    );
-  }
-}
+const App = () => {
+  return (
+    <ErrorBoundary>
+      <MyContent />
+    </ErrorBoundary>
+  );
+};
 
 export default App;
