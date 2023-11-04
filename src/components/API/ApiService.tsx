43693473b92727c1727d0efbff896ea92a062d4e
Duxcoder <iinegve_25@mail.ru>
2023-11-04 23:06:30 +0300

    refactor: correct and optimize code ApiService

--- src/components/API/ApiService.tsx
+++ src/components/API/ApiService.tsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import { API_SERVICE_URL } from '../../settings';
+import { API_SERVICE_URL, Categories } from '../../settings';
 import { CardsPages } from '../../types';
+import { fillArray } from '../../utils/utils';
 
 export async function getData(query: string) {
   try {
     const response = await axios.get(API_SERVICE_URL + query);
-    return response.data.count
-      ? { data: response.data.results, allCount: response.data.count }
-      : null;
+    const { count, results } = response.data;
+    return count ? { data: results, allCount: count } : null;
   } catch (e) {
     if (typeof e === 'string') {
       throw new Error(e.toUpperCase());
@@ -17,19 +17,19 @@ export async function getData(query: string) {
   }
 }
 
-export async function getSearchData(category: string, text: string, page: number = 1) {
+export async function getSearchData(category: Categories, text: string, page: number = 1) {
   return await getData(`${category}/?search=${text}&page=${page}`);
 }
 
 export async function getCardsPages(
-  category: string,
+  category: Categories,
   text: string,
   pages: number = 1,
   startPage: number = 1
 ) {
-  const requests = new Array(pages)
-    .fill(0)
-    .map((num, i) => getSearchData(category, text, startPage + num + i));
+  const requests = fillArray(pages, 0).map((num, i) =>
+    getSearchData(category, text, startPage + num + i)
+  );
   const responses = await Promise.all(requests);
 
   const cardsPages: CardsPages = { data: [], allCount: 0 };
