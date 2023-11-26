4f7de5dcaa7a9a79c7cd93b67da22f2a3df58aac
Duxcoder <iinegve_25@mail.ru>
2023-11-26 20:24:47 +0300

    feat: add close mainCard functionality

--- components/MainCard/MainCard.tsx
+++ components/MainCard/MainCard.tsx
@@ -4,12 +4,25 @@ import { checkNotFoundText } from '../../utils/utils';
 import cl from './MainCard.module.css';
 
 import { CardCharacterCategory } from '@myTypes/main';
+import { useRouter } from 'next/router';
 
 const MainCard = (props: { card: CardCharacterCategory }) => {
+  const router = useRouter();
+
+  const closeMainCard = () => {
+    const newQuery = { ...router.query };
+    delete newQuery.slug;
+    const newUrl = {
+      pathname: '/',
+      query: newQuery,
+    };
+    router.push(newUrl);
+  };
+
   const card = Object.entries(props.card ?? [['error', 'Data is not found']]);
   return (
     <div className={cl.card} data-testid="main-card">
-      <BaseButton circle close callback={() => {}}>
+      <BaseButton circle close callback={() => closeMainCard()}>
         <RiCloseLine />
       </BaseButton>
       {card.map(([name, value], i) => {
