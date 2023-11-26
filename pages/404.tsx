0c625589e503149f55d5ff9ac828a77aa392faa8
Duxcoder <iinegve_25@mail.ru>
2023-11-26 20:39:47 +0300

    fix: errors for build

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,10 @@ import cl from './404.module.css';
 const NotFoundPage = () => {
   return (
     <div className={cl.container}>
-      <h1>404 Not Found</h1>
-      <p>Sorry, the page you are looking for might be in another galaxy.</p>
+      <h1 className={cl.title}>404 Not Found</h1>
+      <p className={cl.paragraph}>
+        Sorry, the page you are looking for might be in another galaxy.
+      </p>
     </div>
   );
 };
